47d02984c9076e5ca8f788db7fe8717e11090077
Maya Keeley <maya.keeley@Nology.local>
2019-12-03 16:10:36 +0000

    data file added

---
+++ data.js
@@ -0,0 +1,85 @@
+export const myQuestions = [
+  {
+    question:
+      "What type of 'ware' is how different applications talk to each other?",
+    answers: {
+      a: "serviceware",
+      b: "mediumware",
+      c: "software",
+      d: "middleware"
+    },
+    correctAnswer: "d"
+  },
+  {
+    question: "What 'aas' is aws an example of?",
+    answers: {
+      a: "SaaS",
+      b: "Paas",
+      c: "Iaas",
+      d: "Baas"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question:
+      "Hashing is a way of encrypting things. Is it one way or two way?",
+    answers: {
+      a: "one way",
+      b: "two way"
+    },
+    correctAnswer: "a"
+  },
+  {
+    question:
+      "What do you tell yarn when downloading things, to obtain that precise version of the thing?",
+    answers: {
+      a: "--exact",
+      b: "--precise",
+      c: "--this",
+      d: "--version"
+    },
+    correctAnswer: "a"
+  },
+  {
+    question:
+      "What is the name for a highly structured database, wherein some data is connected with other data, often represented in a table?",
+    answers: {
+      a: "Non-Relational",
+      b: "Risotto",
+      c: "Relational",
+      d: "Comparative"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "What does the P stand for in PaaS?",
+    answers: {
+      a: "Penne",
+      b: "Platform",
+      c: "Portal",
+      d: "Packages"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "Things outside of our app are accessed...",
+    answers: {
+      a: "synchronously",
+      b: "syntactically",
+      c: "spaghetti",
+      d: "asynchronously"
+    },
+    correctAnswer: "d"
+  },
+  {
+    question:
+      "In promises, then is used when things go right. What is used when things go wrong?",
+    answers: {
+      a: "Seize",
+      b: "Fettucine",
+      c: "Catch",
+      d: "Fetch"
+    },
+    correctAnswer: "c"
+  }
+];
